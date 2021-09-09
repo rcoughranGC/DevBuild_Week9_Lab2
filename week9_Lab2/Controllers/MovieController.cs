9d84090cf7143b23552e89d1ca74713074c3a5c2
Ryan Coughran <ryancoughran@quickenloans.com>
2021-09-09 14:37:36 -0700

    Initial commit

---
+++ week9_Lab2/Controllers/MovieController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using week9_Lab2.Models;
+
+namespace week9_Lab2.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+
+        //public IActionResult MyPage()
+        //{
+        //    ViewData["ActionMovie"] = "Die Hard";
+        //    ViewData["HorrorMovie"] = "The Shining";
+        //    ViewData["ComedyMovie"] = "Caddyshack";
+        //    return View();
+        //}
+        //public IActionResult MyPagee(string actionMov, string horrorMov, string comedyMov)
+        //{
+        //    ViewData["ActionMovie"] = actionMov;
+        //    ViewData["HorrorMovie"] = horrorMov;
+        //    ViewData["ComedyMovie"] = comedyMov;
+        //    return View();
+        //}
+
+        [HttpPost]
+        public IActionResult MyPage(Movies movies)
+        {
+            return View(movies);
+        }
+
+    }
+}
---
+++ week9_Lab2/Models/Movies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace week9_Lab2.Models
+{
+    public class Movies
+    {
+        public string ActionMovie { get; set; }
+        public string HorrorMovie { get; set; }
+        public string ComedyMovie { get; set; }
+    }
+}
